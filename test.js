f6af2b7c3f7ca97e7264e4df2bd06ad68d5241dd
Alex Ahn <alexahn@ahn.local>
2012-07-13 15:08:22 -0400

    minor

--- test.js
+++ test.js
@@ -2,6 +2,29 @@ var mermin = require('./index'),
     csso = require('csso'),
     fs = require('fs');
 
+// notes : extend mermin before calling init, otherwise the tree will not contain any of the extended types
+
+// mermin can be extended to support other file types by using the extend method
+// the extend method takes as arguments a string for the file extension string, and a function for the processor
+// the processor function takes as arguments a string for the merged data, a string for the write_path, and a boolean for minify
+
+/*
+mermin.extend('css', function (data, write_path, minify) {
+    if (minify) {
+        var final_output = csso.justDoIt(data);
+        fs.writeFile(write_path, final_output, cb);
+    }
+    else {
+        fs.writeFile(write_path, data, cb);
+    }
+});
+*/
+
+// output directory for each merged group of leaves is the directory of the first item for the category of each type
+// in our example, it would be /test for both js and css
+// if you wish, you can specify a file that doesn't exist in the desired directory to output to that directory
+// the tree can be flexibly defined so that types and categories can interexchanged as attributes
+
 var tree = 
 {
     'js' : {
@@ -14,6 +37,13 @@ var tree =
             'test/style1.css'
         ]
     },
+    /*
+    'less' : {
+        'project1' : [
+            'test/style1.less'
+        ]
+    },
+    */
     'project1' : {
         'js' : [
             'test/file2.js'
@@ -21,21 +51,13 @@ var tree =
         'css' : [
             'test/style2.css'
         ]
+        /*,
+        'less' : [
+            'test/style2.less'
+        ]
+        */
     }
 };
-
-// tested if extends works by commenting out base css type in mermin
-/*
-mermin.extend('css', function (data, write_path, minify, cb) {
-    if (minify) {
-        var final_output = csso.justDoIt(data);
-        fs.writeFile(write_path, final_output, cb);
-    }
-    else {
-        fs.writeFile(write_path, data, cb);
-    }
-});
-*/
 
 mermin.init(__dirname + '/', tree);
 
